2666637e5b90d007c4e1e867214e835fc9d7e1e5
chebiN <seguido@users.noreply.github.com>
2022-03-20 00:26:56 -0300

    changes

--- src/action.js
+++ src/action.js
@@ -1,6 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const { Octokit } = require("@octokit/rest");
+
 async function run() {
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
     console.log('Attempting sync!');
     console.log('branching');
+    console.log(pull_request.number)
+
+    // const octokit = new Octokit(GITHUB_TOKEN);
+
+    // octokit.repos.
+
 }
 
 run();
