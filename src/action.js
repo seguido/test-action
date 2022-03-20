90874788435a7ba821f015a3f7b5e4d88b5e3c23
chebiN <seguido@users.noreply.github.com>
2022-03-20 01:15:15 -0300

    test30

--- src/action.js
+++ src/action.js
@@ -1,15 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
 
-const { Octokit } = require("@octokit/rest");
+// const { Octokit } = require("@octokit/rest");
+
+const shell = require('shelljs');
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    // const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
 
 
-    const { context = {} } = github;
-    const { pull_request, repository } = context.payload;
+    // const { context = {} } = github;
+    // const { pull_request, repository } = context.payload;
     // console.log('context')
     // console.log(context);
     // console.log('---')
@@ -24,16 +26,19 @@ async function run() {
 
     
 
-    const octokit = new Octokit(GITHUB_TOKEN);
+    // const octokit = new Octokit(GITHUB_TOKEN);
 
-    let resp = await octokit.pulls.listFiles({
-        owner: repository.full_name.split('/')[0], 
-        repo: repository.full_name.split('/')[1],
-        pull_number: pull_request.number
-    });
+    // let resp = await octokit.pulls.listFiles({
+    //     owner: repository.full_name.split('/')[0], 
+    //     repo: repository.full_name.split('/')[1],
+    //     pull_number: pull_request.number
+    // });
 
-    console.log(resp);
+    // console.log(resp);
 
+    shell.pwd();
+    shell.ls();
+    console.log(shell.ls());
 }
 
 run();
