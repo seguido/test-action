a8fa2989348cbc077787f0cac03255e429682b24
chebiN <17909998+seguido@users.noreply.github.com>
2022-03-24 19:36:17 -0300

    full test

--- src/action.js
+++ src/action.js
@@ -8,46 +8,42 @@ async function run() {
 
   const { context = {} } = github;
 
-//   console.log(context)
-  console.log('------')
-  console.log(context.sha)
+  const commit = context.sha;
 
-  console.log('hello')
-  //Create repos folder
-//   shell.cd('..');
-//   shell.exec('mkdir repos');
-//   shell.cd('./repos');
-//   //Clone
-//   shell.exec('git clone https://github.com/beefyfinance/beefy-app');
-//   shell.cd('./beefy-app');
-//   shell.exec('git checkout prod');
-//   shell.cd('..');
-//   shell.exec(`git clone https://${GITHUB_TOKEN}@github.com/beefyfinance/beefy-v2`);
-//   shell.cd('./beefy-v2');
-//   shell.exec('yarn install');
-//   //Sync
-//   shell.exec('yarn sync');
+  // Create repos folder
+  shell.cd('..');
+  shell.exec('mkdir repos');
+  shell.cd('./repos');
+  //Clone
+  shell.exec('git clone https://github.com/beefyfinance/beefy-app');
+  shell.cd('./beefy-app');
+  shell.exec('git checkout prod');
+  shell.cd('..');
+  shell.exec(`git clone https://${GITHUB_TOKEN}@github.com/beefyfinance/beefy-v2`);
+  shell.cd('./beefy-v2');
+  shell.exec('yarn install');
+  //Sync
+  shell.exec('yarn sync');
 
-//   //Check for modded files
-//   let out = shell.exec('git status');
-//   let modified = out
-//     .grep('modified')
-//     .split('\n')
-//     .filter(line => line.includes('src/') || line.includes('package.json'));
-//   console.log(modified);
-//   if (modified.length > 0) {
-//     console.log('Modified files, commiting and syncing');
-//     const branch = `autosync/${pull_request.number}${Math.random() * 45}`;
-//     shell.exec('git add .');
-//     shell.exec(`git checkout -b ${branch}`);
-//     shell.exec(`git config user.email "chebiN@beefy.com"`);
-//     shell.exec(`git config user.name "chebiN"`);
-//     shell.exec(`git commit -m 'sync'`);
-//     shell.exec('git push');
-//     // shell.exec(`git push --set-upstream origin ${branch}`);
-//   } else {
-//     console.log('Nothing to commit');
-//   }
+  //Check for modded files
+  let out = shell.exec('git status');
+  let modified = out
+    .grep('modified')
+    .split('\n')
+    .filter(line => line.includes('src/') || line.includes('package.json'));
+  console.log(modified);
+  if (modified.length > 0) {
+    console.log('Modified files, commiting and syncing');
+    const branch = `as/${commit}`;
+    shell.exec('git add .');
+    shell.exec(`git checkout -b ${branch}`);
+    shell.exec(`git config user.name "chebiN"`);
+    shell.exec(`git commit -m 'sync'`);
+    shell.exec('git push');
+    shell.exec(`git push --set-upstream origin ${branch}`);
+  } else {
+    console.log('Nothing to commit');
+  }
 }
 
 run();
